407fb12a63703da90a4448d0b923bd448d9ca7ac
Vineet <vineetprashant1@gmail.com>
2024-07-31 19:30:32 +0530

    ai-server updated

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
 const URL = "https://onlybots.onrender.com/";
-const URL_AI = "https://onlybotsai.onrender.com/";
+const URL_AI = "https://onlybots-fdzi.onrender.com/";
 const RATE_LIMIT_RPM = 1;
 export { URL, URL_AI, RATE_LIMIT_RPM };
